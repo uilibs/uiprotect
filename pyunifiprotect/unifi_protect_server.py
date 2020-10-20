61ae4b6603b7d4e090dd37f601f11d89b51628b0
Bjarne Riis <bjarne@briis.com>
2020-10-20 15:55:22 +0200

    Added Service to set LCD message

--- pyunifiprotect/unifi_protect_server.py
+++ pyunifiprotect/unifi_protect_server.py
@@ -733,6 +733,27 @@ class UpvServer:
                     % (response.status, response.reason)
                 )
 
+    async def set_doorbell_custom_text(self, camera_id: str, custom_text: str) -> bool:
+        """Sets a Custom Text string for the Doorbell LCD'."""
+
+        await self.ensureAuthenticated()
+
+        message_type = "CUSTOM_MESSAGE"
+
+        cam_uri = f"{self._base_url}/{self.api_path}/cameras/{camera_id}"
+        data = {"lcdMessage": {"type": message_type, "text": custom_text, "resetAt": None}}
+
+        async with self.req.patch(
+            cam_uri, headers=self.headers, verify_ssl=self._verify_ssl, json=data
+        ) as response:
+            if response.status == 200:
+                return True
+            else:
+                raise NvrError(
+                    "Setting Doorbell Custom Text failed: %s - Reason: %s"
+                    % (response.status, response.reason)
+                )
+
     async def request(self, method, url, json=None, **kwargs):
         """Make a request to the API."""
 
