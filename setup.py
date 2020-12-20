7a89dcec0d1f764b0e566decbe12dde467de9faa
Bjarne Riis <bjarne@briis.com>
2020-12-20 06:12:11 +0100

    Bumped to 0.27.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'pyunifiprotect',
   packages = ['pyunifiprotect'],
-  version = '0.26.8',
+  version = '0.27.0',
   license='MIT',
   description = 'Python Wrapper for Unifi Protect API', 
   author = 'Bjarne Riis',
