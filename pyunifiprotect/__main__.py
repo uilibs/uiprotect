804c58b2913d9a6c9360b1a87b0afd8985ad724f
Christopher Bailey <cbailey@mort.is>
2022-06-03 16:39:47 -0400

    Bumps version to 3.8.0. Removes unused packages from setup

--- pyunifiprotect/__main__.py
+++ pyunifiprotect/__main__.py
@@ -1,16 +1,20 @@
 import os
 
-from dotenv import load_dotenv
+try:
+    from dotenv import load_dotenv
+except ImportError:
+    load_dotenv = None  # type: ignore
 
 from pyunifiprotect.cli import app
 
 
 def start() -> None:
-    env_file = os.path.join(os.getcwd(), ".env")
-    if os.path.exists(env_file):
-        load_dotenv(dotenv_path=env_file)
-    else:
-        load_dotenv()
+    if load_dotenv is not None:
+        env_file = os.path.join(os.getcwd(), ".env")
+        if os.path.exists(env_file):
+            load_dotenv(dotenv_path=env_file)
+        else:
+            load_dotenv()
     app()
 
 
