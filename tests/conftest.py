f51394c0b1bd7f2bf3fed32f06dc4ff2c605fc94
Christopher Bailey <cbailey@mort.is>
2024-03-21 15:22:49 -0400

    ignore bridge parameter on sensor for now

--- tests/conftest.py
+++ tests/conftest.py
@@ -749,6 +749,7 @@ def compare_objs(obj_type, expected, actual):  # noqa: PLR0915
         del expected["bridgeCandidates"]
         actual.pop("host", None)
         expected.pop("host", None)
+        expected.pop("bridge", None)
     elif obj_type == ModelType.CHIME.value:
         del expected["apMac"]
         del expected["apRssi"]
