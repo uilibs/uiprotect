b2e4e8ef3536bbedc8d3765afb4fd3cb45b478ba
J. Nick Koston <nick@koston.org>
2024-06-09 12:45:20 -0500

    fix: cli test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -9,4 +9,4 @@ def test_help():
     """The help message includes the CLI name."""
     result = runner.invoke(app, ["--help"])
     assert result.exit_code == 0
-    assert "Add the arguments and print the result" in result.stdout
+    assert "UniFi Protect CLI" in result.stdout
