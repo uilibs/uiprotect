8d82d831e49f107762e036e392c494abb1aea632
J. Nick Koston <nick@koston.org>
2024-10-05 20:24:51 -0500

    Update src/uiprotect/_compat.py
    Signed-off-by: J. Nick Koston <nick@koston.org>

--- src/uiprotect/_compat.py
+++ src/uiprotect/_compat.py
@@ -3,6 +3,6 @@
 try:
     from propcache.api import cached_property
 except ImportError:
-    from ._compat import cached_property
+    from propcache import cached_property
 
 __all__ = ("cached_property",)
